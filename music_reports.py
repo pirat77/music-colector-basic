ec3d441eba9eb3697f29e85ac876390e36133335
pirat77 <mcyanek@gmail.com>
2020-01-26 15:33:59 +0100

    Reading file into data structure

---
+++ music_reports.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+path = sys.argv[0].strip("music_reports.py")
+data_structure = ['artist name', 'album name', 'release year', 'genre', 'length']
+albums_dict =[]
+print(path)
+
+
+line = 0
+with open(path + 'text_albums_data.txt', 'r') as f:
+    albums_list = f.readlines()
+
+for element in albums_list:
+    albums_dict.append(dict(zip(data_structure, element.strip("\n").split(','))))
+
+
+print(albums_dict)
+

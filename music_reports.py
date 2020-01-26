5044a3df7783ffd4d16738d774f2e0ab1c2ffe69
pirat77 <mcyanek@gmail.com>
2020-01-26 15:40:16 +0100

    Edited reading file into data structure into readfile() function

--- music_reports.py
+++ music_reports.py
@@ -1,19 +1,18 @@
 import sys
 import re
 
-path = sys.argv[0].strip("music_reports.py")
-data_structure = ['artist name', 'album name', 'release year', 'genre', 'length']
-albums_dict =[]
-print(path)
 
 
-line = 0
-with open(path + 'text_albums_data.txt', 'r') as f:
-    albums_list = f.readlines()
+def readfile():
+    path = sys.argv[0].strip("music_reports.py")
+    data_structure = ['artist name', 'album name', 'release year', 'genre', 'length']
+    albums_dict =[]
+    with open(path + 'text_albums_data.txt', 'r') as f:
+        albums_list = f.readlines()
+    for element in albums_list:
+        albums_dict.append(dict(zip(data_structure, element.strip("\n").split(','))))
+    return albums_dict
 
-for element in albums_list:
-    albums_dict.append(dict(zip(data_structure, element.strip("\n").split(','))))
+print(readfile())
 
-
-print(albums_dict)
 
